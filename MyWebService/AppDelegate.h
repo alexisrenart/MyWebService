f5146ab429fb7297c0c1f0798d4ae036586013cc
Alexis RENART <Alexis@MacBook-Pro-de-Alexis.local>
2014-04-15 21:50:32 +0200

    Initial Commit

---
+++ MyWebService/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyWebService
+//
+//  Created by Alexis RENART on 15/04/2014.
+//  Copyright (c) 2014 Alexis RENART. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MyWebService/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MyWebService
+//
+//  Created by Alexis RENART on 15/04/2014.
+//  Copyright (c) 2014 Alexis RENART. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
